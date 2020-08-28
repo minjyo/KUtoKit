c8f9914e5c9f27720202717a87b1478b991fe0b4
minjyo <minjyo8823@gmail.com>
2020-08-28 17:50:44 +0900

    initial setting

---
+++ src/kutokit/MainApp.java
@@ -0,0 +1,77 @@
+package kutokit;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+	
+	 private Stage primaryStage;
+	 private BorderPane rootLayout;
+	 
+	@Override
+	//main에 앱이 실행되면 자동으로 호출
+	public void start(Stage primaryStage) {
+		 this.primaryStage = primaryStage;
+	        this.primaryStage.setTitle("Kutokit");
+
+	        initRootLayout();
+
+	        showView();
+	        //minjyo commit!
+	}
+
+	 /**
+     * 상위 레이아웃을 초기화한다.
+     */
+	private void initRootLayout() {
+		try {
+            // fxml 파일에서 상위 레이아웃을 가져온다.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainApp.class.getResource("view/RootLayout.fxml"));
+            rootLayout = (BorderPane) loader.load();
+
+            // 상위 레이아웃을 포함하는 scene을 보여준다.
+            Scene scene = new Scene(rootLayout);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }	
+	}
+	
+	/**
+     * 상위 레이아웃 안에 연락처 요약(person overview)을 보여준다.
+     */
+    public void showView() {
+        try {
+            // 연락처 요약을 가져온다.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainApp.class.getResource("view/View.fxml"));
+            AnchorPane View = (AnchorPane) loader.load();
+
+            // 연락처 요약을 상위 레이아웃 가운데로 설정한다.
+            rootLayout.setCenter(View);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+	 * 메인 스테이지를 반환한다.
+	 * @return
+	 */
+	public Stage getPrimaryStage() {
+		return primaryStage;
+	}
+
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
