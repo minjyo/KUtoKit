ada5bd626ede66bf23ec5d12ccbf3f7b7cebbd28
JeonDY <pl04074@naver.com>
2020-09-07 20:26:41 +0900

    Revert "0907"
    This reverts commit 7113627ce5ad05d4a34e253f7d7e20d8a3eeffd1.

--- src/kutokit/view/UtmController.java
+++ src/kutokit/view/UtmController.java
@@ -1,34 +1,18 @@
 package kutokit.view;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
 import kutokit.MainApp;
 
 public class UtmController {
-	@FXML private TableView contextTable;
-	@FXML private TableColumn<String,String> ControlAction, Providing, IncorrectTime, StoppedTooLong, notProviding;
-
 
 	private MainApp mainApp;
-
+	
 	//constructor
 	public UtmController() {
-
+		
 	}
 
 	//set MainApp
 	public void setMainApp(MainApp mainApp) {
 		this.mainApp = mainApp;
-	}
-
-	public void clearTable()
-	{
-
-	}
-
-	public void addCase(String CAC,String cases, String no,String context)
-	{
-
 	}
 }
