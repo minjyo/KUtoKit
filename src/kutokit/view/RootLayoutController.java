671d2247ef3b52bbe7541744bda7c626ce41a5fb
Yeon Ji <dearyeon>
2020-09-07 09:46:56 +0900

    openfile 기능 추가

--- src/kutokit/view/RootLayoutController.java
+++ src/kutokit/view/RootLayoutController.java
@@ -3,6 +3,17 @@ package kutokit.view;
 import javafx.fxml.FXML;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
 import kutokit.MainApp;
 
 public class RootLayoutController {
@@ -10,6 +21,7 @@ public class RootLayoutController {
 	private MainApp mainApp;
 	private TabPane mainTab;
 	private Tab cseTab;
+	private File selectedFile;
 	
 	//constructor
 	public RootLayoutController() {
@@ -41,4 +53,38 @@ public class RootLayoutController {
 	private void handleUtmButton() {
 		this.mainApp.showUtmView();
 	}
+	
+	
+
+    @FXML
+    private void handleOpen() {
+        FileChooser fileChooser = new FileChooser();
+
+        // 확장자 필터를 설정한다.
+        FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter(
+                "XML files (*.xml)", "*.xml");
+        fileChooser.getExtensionFilters().add(extFilter);
+
+        // Save File Dialog를 보여준다.
+        File file = fileChooser.showOpenDialog(mainApp.getPrimaryStage());
+
+        if (file != null) {
+            /*
+             * 파싱코드
+             * 
+             * 
+             */	
+        }
+    }
+/*
+    @FXML
+    private void handleSave() {
+        File personFile = mainApp.getPersonFilePath();
+        if (personFile != null) {
+            mainApp.savePersonDataToFile(personFile);
+        } else {
+            handleSaveAs();
+        }
+    }
+*/
 }
