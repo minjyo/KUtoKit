c70d61c30968e580c6d5f9af628d6e7d62cffc22
minjyo <minjyo8823@gmail.com>
2020-11-02 18:24:53 +0900

    chore: Set ArrowView z-index < RectangleView z-index

--- src/kutokit/view/CseController.java
+++ src/kutokit/view/CseController.java
@@ -98,10 +98,10 @@ public class CseController {
 				}
 			}
 			ArrowView a = new ArrowView(ca, startX, startY, endX,  endY, ca.getId());
+			a.toBack();
 			LabelView label = new LabelView(a.startX, a.startY, a.endX, a.endY, ca.getCA(), "CA");
 			a.setLabel(label);
 			
-			System.out.println(ca.getControllerID());
 			dataStore.findController(ca.getControllerID()).addCA(ca.getId(), 1);
 			dataStore.findController(ca.getControlledID()).addCA(ca.getId(), 0);
 			
@@ -274,6 +274,7 @@ public class CseController {
 		s.setId(Integer.toString(c.getId()));
 
 		root.getChildren().add(s);
+		//s.toFront();
 	}
 
 	private void addControlAction(Path s, Label l, ControlAction ca) {
@@ -291,6 +292,7 @@ public class CseController {
 		l.setId("-1");
 		
 		root.getChildren().addAll(s, l);
+		s.toBack();
 	}
 	
 	private void addFeedback(Path s, Label l, Feedback fb) {
@@ -308,6 +310,7 @@ public class CseController {
 		l.setId("-1");
 		
 		root.getChildren().addAll(s, l);
+		s.toBack();
 	}
 
 	private void modifyControllerPopUp(RectangleView rect) {
