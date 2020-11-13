44327219128a4160001d8d3d78504e8a4a2bb57e
dearyeon <d_earyeon@naver.com>
2020-11-13 12:05:17 +0900

    test

--- src/kutokit/model/ctm/CTMDataStore.java
+++ src/kutokit/model/ctm/CTMDataStore.java
@@ -6,7 +6,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
 public class CTMDataStore {
-
+ int test;
 	ArrayList<ObservableList<CTM>> CTMTableList = new ArrayList<>();
 
 	public ArrayList<ObservableList<CTM>> getCTMTableList() {
